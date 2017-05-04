da4fad5f0cab7568b39d9d20dc3c40d007a08166
JAIME AGUILAR <jaimemets@gmail.com>
2017-05-03 20:14:06 -0500

    Categoria de una plantilla

---
+++ Application/Core/development/src/com/cobus/dynamictemplate/util/CategoryTemplate.java
@@ -0,0 +1,48 @@
+package com.cobus.dynamictemplate.util;
+
+/**
+ * Title: CategoryTemplate.java
+ * @author jaguilar (JAR)
+ * File Creation on 24/04/2016
+ */
+public enum CategoryTemplate {
+    
+    GENERIC(1, "label.generic");
+    
+    private int value;
+    private String name;
+
+    private CategoryTemplate(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public static CategoryTemplate valueOf(int value) {
+        for (CategoryTemplate ct : values()) {
+            if (ct.getValue() == value) {
+                return ct;
+            }
+        }
+        return null;
+    }
+    
+    
+    
+
+}
